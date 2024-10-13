9c91ec5aea47e4ff972a00de9beb01362b8df2ec
Slawek84PL <slapyt84@gmail.com>
2024-10-13 13:38:20 +0200

    Added @Repository annotation to UserRepository interface for better Spring component scanning.

--- src/main/java/user/UserRepository.java
+++ src/main/java/user/UserRepository.java
@@ -1,8 +1,10 @@
 package user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 interface UserRepository extends JpaRepository<User, UUID> {
 }
