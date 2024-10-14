5c07e85ba2e4209b39f8c19d2817358554be993b
RobertoJavaDev <robertojavadev@gmail.com>
2024-10-14 09:01:56 +0200

    feat: Updated entities

--- src/main/java/pl/ateam/disasteralerts/disasteralert/Alert.java
+++ src/main/java/pl/ateam/disasteralerts/disasteralert/Alert.java
@@ -1,9 +1,12 @@
 package pl.ateam.disasteralerts.disasteralert;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -25,15 +28,12 @@ class Alert {
     @GeneratedValue
     private UUID id;
 
-    @Column(nullable = false)
-    private DisasterType type;
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Disaster disaster;
 
     @Column(nullable = false)
-    private String location;
+    private String description;
 
     @Column(nullable = false)
     private Instant creationDate;
-
-    @Column(nullable = false)
-    private DisasterStatus status;
 }
--- src/main/java/pl/ateam/disasteralerts/disasteralert/DisasterStatus.java
+++ src/main/java/pl/ateam/disasteralerts/disasteralert/DisasterStatus.java
@@ -1,5 +1,5 @@
 package pl.ateam.disasteralerts.disasteralert;
 
 enum DisasterStatus {
-    ACTIVE, FINISHED
+    ACTIVE, FINISHED, FAKE
 }
