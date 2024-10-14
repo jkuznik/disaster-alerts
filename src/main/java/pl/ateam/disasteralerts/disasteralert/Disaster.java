ff7c8e5b356b917ad5205c363013b83d2aacbb64
RobertoJavaDev <robertojavadev@gmail.com>
2024-10-14 09:01:56 +0200

    feat: Updated entities

--- src/main/java/pl/ateam/disasteralerts/disasteralert/Disaster.java
+++ src/main/java/pl/ateam/disasteralerts/disasteralert/Disaster.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -41,4 +43,7 @@ class Disaster {
 
     @Column(nullable = false)
     private DisasterStatus status;
+
+    @OneToMany
+    private List<Alert> alerts;
 }
