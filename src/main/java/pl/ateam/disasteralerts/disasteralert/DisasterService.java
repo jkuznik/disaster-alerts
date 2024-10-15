a1da974934d7b47e91b861530a7c5e2d7a85151e
jkuznik <janusz.kuznik89@gmail.com>
2024-10-15 15:43:47 +0200

    Fix lombok issue

--- src/main/java/pl/ateam/disasteralerts/disasteralert/DisasterService.java
+++ src/main/java/pl/ateam/disasteralerts/disasteralert/DisasterService.java
@@ -1,21 +1,17 @@
 package pl.ateam.disasteralerts.disasteralert;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pl.ateam.disasteralerts.disasteralert.dto.DisasterAddDTO;
 import pl.ateam.disasteralerts.disasteralert.dto.DisasterDTO;
 
 @Service
+@RequiredArgsConstructor
 class DisasterService implements DisasterServiceAPI{
 
     private final DisasterRepository repository;
     private final DisasterMapper mapper;
-
-
-    public DisasterService(DisasterRepository repository, DisasterMapper mapper) {
-        this.repository = repository;
-        this.mapper = mapper;
-    }
 
     @Transactional
     public DisasterDTO addDisaster(DisasterAddDTO disasterAddDTO){
