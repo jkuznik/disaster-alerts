a1a8ac99e14ea7aa078ccc2b9f8ef7f1e0095d91
jkuznik <janusz.kuznik89@gmail.com>
2024-10-16 00:59:09 +0200

    Get username from security context implementation

--- src/main/java/pl/ateam/disasteralerts/disasteralert/DisasterController.java
+++ src/main/java/pl/ateam/disasteralerts/disasteralert/DisasterController.java
@@ -2,6 +2,10 @@ package pl.ateam.disasteralerts.disasteralert;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,9 +21,25 @@ class DisasterController {
 
     private final DisasterService disasterService;
 
+    // TODO: wymaganie postawione dla implementacji security
+    //  Obiekt Authentication w Credentials, Details lup Principal musi posiadać jedno z pól User będące jako UNIQUE w bazie danych
+
     @PostMapping(DISASTERS_BASE_URL)
-    public ResponseEntity<DisasterDTO> createDisaster(@RequestParam DisasterType disasterType,
+    public ResponseEntity<DisasterDTO> createDisaster(@AuthenticationPrincipal Authentication authentication,
+                                                      @RequestParam DisasterType disasterType,
                                                       @RequestParam String description) {
+
+        // w zależności od implementacji security skorzysta się z jednej z trzech metod poniżej
+        // roboczo posłużę się metodą getDetails();
+        Object credentials = authentication.getCredentials();
+        Object details = authentication.getDetails();
+        Object principal = authentication.getPrincipal();
+
+        UserDetails userDetails = (UserDetails) details;
+
+        String username = userDetails.getUsername();
+
+
 
         return null;
     }
