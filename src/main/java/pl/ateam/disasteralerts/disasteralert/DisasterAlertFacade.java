d6e9c5f9ca757b53fe255c9a8e42526adf9b6b3b
jkuznik <janusz.kuznik89@gmail.com>
2024-10-15 21:32:43 +0200

    Use lombok annotation in DisasterAlertFacade

--- src/main/java/pl/ateam/disasteralerts/disasteralert/DisasterAlertFacade.java
+++ src/main/java/pl/ateam/disasteralerts/disasteralert/DisasterAlertFacade.java
@@ -2,6 +2,7 @@ package pl.ateam.disasteralerts.disasteralert;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 import pl.ateam.disasteralerts.disasteralert.dto.DisasterAddDTO;
@@ -9,13 +10,10 @@ import pl.ateam.disasteralerts.disasteralert.dto.DisasterDTO;
 
 @Component
 @Validated
+@RequiredArgsConstructor
 public class DisasterAlertFacade {
 
     private final DisasterService disasterService;
-
-    public DisasterAlertFacade(DisasterService disasterService) {
-        this.disasterService = disasterService;
-    }
 
     public DisasterDTO addDisaster(@NotNull @Valid DisasterAddDTO disasterAddDTO) {
         return disasterService.addDisaster(disasterAddDTO);
