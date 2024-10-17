97da1b47e0d5444e9f58f22f7c1b2e4be450ff10
Slawek84PL <slapyt84@gmail.com>
2024-10-17 23:03:48 +0200

    feat: update security configuration to allow public access to signup page

--- src/main/java/pl/ateam/disasteralerts/security/SecurityConfig.java
+++ src/main/java/pl/ateam/disasteralerts/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package pl.ateam.disasteralerts.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,14 +18,16 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize  -> authorize
-                        .requestMatchers("/").permitAll()
-                        .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
-                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/signup").permitAll()
+//                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(loginConfigurer -> loginConfigurer
+                        .loginPage("/login").permitAll()
+                        .defaultSuccessUrl("/signup"))
+                .logout(logout -> logout.logoutSuccessUrl("/login"))
                 .authenticationProvider(customDaoAuthenticationProvider);
-//                .formLogin(loginConfigurer -> loginConfigurer
-//                        .loginPage("/login").permitAll());
         return http.build();
     }
 }
