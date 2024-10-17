fc65165f245b4ddc8477aa8ffffe6de596a62e2e
jkuznik <janusz.kuznik89@gmail.com>
2024-10-17 05:38:08 +0200

    Add test template for DisasterController

---
+++ src/test/java/pl/ateam/disasteralerts/disasteralert/DisasterControllerTest.java
@@ -0,0 +1,70 @@
+package pl.ateam.disasteralerts.disasteralert;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import pl.ateam.disasteralerts.disasteralert.dto.DisasterAddDTO;
+import pl.ateam.disasteralerts.disasteralert.dto.DisasterDTO;
+
+import java.time.Instant;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(DisasterController.class)
+@Import(DisasterMapperImpl.class)
+class DisasterControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    DisasterMapper mapper;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    DisasterService disasterService;
+
+    @MockBean
+    Authentication authentication;
+
+    private final DisasterAddDTO disasterAddDTO = new DisasterAddDTO(
+            DisasterType.FLOOD,
+            "foo",
+            "bar",
+            "bazz",
+            Instant.now(),
+            DisasterStatus.ACTIVE,
+            "testEmail");
+
+    @Nested
+    class POSTMethodsTest{
+        @Test
+        void createDisaster() throws Exception {
+            //given
+            authentication.setAuthenticated(true);
+            DisasterDTO disasterDTO = mapper.mapDisasterToDisasterDto(mapper.mapDisasterAddDtoToDisaster(disasterAddDTO));
+
+            //when
+            when(authentication.getDetails()).thenReturn("testEmail");
+            when(disasterService.addDisaster(any(DisasterAddDTO.class))).thenReturn(disasterDTO);
+
+            //then
+            mockMvc.perform(MockMvcRequestBuilders.post(DisasterController.DISASTERS_BASE_URL)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(disasterAddDTO)))
+                        .andExpect(status().isCreated());
+        }
+    }
+}
