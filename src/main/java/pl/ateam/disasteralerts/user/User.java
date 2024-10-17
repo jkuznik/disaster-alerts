dfe31172017e809649d8ecfdb068a6f52c7f8f02
YamiHeike <koscielniak.z@gmail.com>
2024-10-18 00:37:09 +0200

    added User DTO validation

--- src/main/java/pl/ateam/disasteralerts/user/User.java
+++ src/main/java/pl/ateam/disasteralerts/user/User.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Enumerated;
 
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -29,7 +30,7 @@ class User extends EntityAudit {
     @Size(min = 5, max = 15)
     @Column(unique = true, length = 15)
     private String username;
-
+    @Min(6)
     private String password;
 
     @Email
--- src/main/java/pl/ateam/disasteralerts/user/dto/UserDTO.java
+++ src/main/java/pl/ateam/disasteralerts/user/dto/UserDTO.java
@@ -1,11 +1,24 @@
 package pl.ateam.disasteralerts.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import pl.ateam.disasteralerts.user.Role;
 
-public record UserDTO(String username,
-                      String email,
-                      String password,
-                      String phoneNumber,
-                      String location,
-                      String role) {
+public record UserDTO(
+                        @Size(min=5, max=15)
+                        @NotBlank
+                        String username,
+                        @Email @NotBlank
+                        @Max(255)
+                        String email,
+                        @NotBlank @Size(min = 6, max = 255)
+                        String password,
+                        @Size(min=9,max=9)
+                        String phoneNumber,
+                        @Max(255)
+                        String location,
+                        String role) {
 }
