73cd1711ccff79ff324f4ef75ab3ae0b1983bbca
jkuznik <janusz.kuznik89@gmail.com>
2024-10-18 04:18:50 +0200

    Delete unnecessary comment

--- src/main/java/pl/ateam/disasteralerts/disasteralert/dto/AlertDTO.java
+++ src/main/java/pl/ateam/disasteralerts/disasteralert/dto/AlertDTO.java
@@ -2,7 +2,7 @@ package pl.ateam.disasteralerts.disasteralert.dto;
 
 import java.time.Instant;
 import java.util.UUID;
-//TODO: moja propozycja zmiany pola Disaster disaster na UUID disasterId - wtedy można przenieść DTO do pakietu dto
+
 public record AlertDTO(UUID id,
                 UUID disasterId,
                 String location,
