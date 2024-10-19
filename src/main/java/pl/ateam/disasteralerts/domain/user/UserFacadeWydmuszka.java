7efe1323783924ecca6c4b3a045371d4ca051ea2
jkuznik <janusz.kuznik89@gmail.com>
2024-10-19 03:08:00 +0200

    getInterestedUsers mock method

--- src/main/java/pl/ateam/disasteralerts/domain/user/UserFacadeWydmuszka.java
+++ src/main/java/pl/ateam/disasteralerts/domain/user/UserFacadeWydmuszka.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Component;
 import pl.ateam.disasteralerts.domain.disaster.enums.DisasterType;
 import pl.ateam.disasteralerts.domain.user.dto.UserDTO;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
@@ -13,8 +15,20 @@ public class UserFacadeWydmuszka {
 
     private final UserService userService;
 
+    public Set<UserDTO> getInterestedUsers(DisasterType disasterType, String location) {
+        Set<UserDTO> usersByDisasterType = getUsersByDisasterType(disasterType);
+        Set<UserDTO> usersByLocation = getUsersByLocation(location);
+
+        Set<UserDTO> interestedUsers = new HashSet<>();
+
+        interestedUsers.addAll(usersByDisasterType);
+        interestedUsers.addAll(usersByLocation);
+        return interestedUsers;
+    }
+
+
     //TODO: propozycja aby uwtorzyc UserEmailDto, obiekt który zwroci sam emil usera
-    List<UserDTO> getUsersByDisasterType(DisasterType disasterType) {
+    private Set<UserDTO> getUsersByDisasterType(DisasterType disasterType) {
         //TODO: user powinien posiadać List<DisasterType> na potrzeby zdefiniowania które zagrożenia go interesują
 
 //        userService.findAllByDisasterType(disasterType);
@@ -22,13 +36,13 @@ public class UserFacadeWydmuszka {
 //        ta metoda będzie pomocna albo inaczej wyciągamy wszystkich użytkowników a potem filtrujemy po polu
 //        DisasterType i zwracamy liste
 //
-        return List.of();
+        return Set.of();
     }
 
-    List<UserDTO> getUsersByLocation(String location) {
+    private Set<UserDTO> getUsersByLocation(String location) {
 
 //        userService.findAllByLocation(location);
 //        ta metoda analogicznie do powyższej
-        return List.of();
+        return Set.of();
     }
 }
