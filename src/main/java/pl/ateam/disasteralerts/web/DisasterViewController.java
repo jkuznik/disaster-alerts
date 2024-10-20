6bae11c6b748a0380b8e72dce035881990a44472
Slawek84PL <slapyt84@gmail.com>
2024-10-20 20:44:43 +0200

    refactor: improve disaster addition workflow and data handling

--- src/main/java/pl/ateam/disasteralerts/web/DisasterViewController.java
+++ src/main/java/pl/ateam/disasteralerts/web/DisasterViewController.java
@@ -3,17 +3,19 @@ package pl.ateam.disasteralerts.web;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.ateam.disasteralerts.disasteralert.DisasterAlertFacade;
 import pl.ateam.disasteralerts.disasteralert.DisasterStatus;
 import pl.ateam.disasteralerts.disasteralert.DisasterType;
-import pl.ateam.disasteralerts.disasteralert.dto.DisasterAddDTO;
+import pl.ateam.disasteralerts.disasteralert.dto.DisasterAddWebDTO;
+import pl.ateam.disasteralerts.security.AppUser;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,26 +26,34 @@ public class DisasterViewController {
 
 
     @GetMapping("add")
-    public String showAddDisasterForm(Model model, @AuthenticationPrincipal UserDetails userDetails) {
+    public String showAddDisasterForm(Model model, @AuthenticationPrincipal AppUser userDetails) {
+
         baseModel(model, userDetails);
-        model.addAttribute("disasterAddDTO", new DisasterAddDTO(null, "", "", "", null, null, ""));
+        model.addAttribute("disasterTypSelected", null);
+        model.addAttribute("disasterAddDTO", new DisasterAddWebDTO(null, null, null, null));
+
         return "addDisaster";
     }
 
     @PostMapping("add")
-    public String addDisaster(@AuthenticationPrincipal UserDetails userDetails,
-                              @Valid DisasterAddDTO disasterAddDTO,
-                              BindingResult result, Model model) {
+    public String addDisaster(Model model, @AuthenticationPrincipal AppUser userDetails,
+                              @Valid @ModelAttribute DisasterAddWebDTO disasterAddWebDTO,
+                              BindingResult result, RedirectAttributes redirectAttributes) {
+
         if (result.hasErrors()) {
             baseModel(model, userDetails);
-            model.addAttribute("disasterAddDTO", disasterAddDTO);
+            model.addAttribute("disasterTypSelected", disasterAddWebDTO.type());
+            model.addAttribute("disasterAddDTO", disasterAddWebDTO);
             return "addDisaster";
         }
         disasterService.addDisaster(disasterAddDTO);
         return "redirect:/disasters";
+
+        redirectAttributes.addFlashAttribute("message", "Dodano zdarzenie");
+        return "redirect:/disasters/add";
     }
 
-    private void baseModel(Model model, UserDetails userDetails) {
+    private void baseModel(Model model, AppUser userDetails) {
         model.addAttribute("email", userDetails.getUsername());
         model.addAttribute("disasterType", DisasterType.values());
         model.addAttribute("disasterStatus", DisasterStatus.values());
