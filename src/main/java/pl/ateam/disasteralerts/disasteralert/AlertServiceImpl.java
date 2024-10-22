a714fe22518f34b6f90273e7eb527acd06fedfbf
jkuznik <janusz.kuznik89@gmail.com>
2024-10-22 19:48:38 +0200

    Logic to achieve send notification

--- src/main/java/pl/ateam/disasteralerts/disasteralert/AlertServiceImpl.java
+++ src/main/java/pl/ateam/disasteralerts/disasteralert/AlertServiceImpl.java
@@ -5,12 +5,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pl.ateam.disasteralerts.disasteralert.dto.AlertAddDTO;
 import pl.ateam.disasteralerts.disasteralert.dto.AlertDTO;
+import pl.ateam.disasteralerts.user.dto.UserDTO;
+
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 class AlertServiceImpl implements AlertService {
 
     private final AlertRepository repository;
+    private final AlertManager alertManager;
+    private final NotificationService notificationService;
     private final AlertMapper mapper;
 
     @Transactional
@@ -19,7 +24,15 @@ class AlertServiceImpl implements AlertService {
         Alert alert = mapper.mapAlertAddDtoToAlert(alertAddDTO);
         AlertDTO alertDTO = mapper.mapAlertToAlertDto(repository.save(alert));
 
+        sendNotifications(alertAddDTO);
         return alertDTO;
     }
 
+    private void sendNotifications(AlertAddDTO alertAddDTO) {
+        String hardCodeLocationForTest = "Warszawa";
+        Set<UserDTO> interestedUsers = userFacadeWydmuszka.getInterestedUsers( /*alertAddDTO.location()*/ hardCodeLocationForTest);
+
+        alertManager.addAlertListener(notificationService);
+        alertManager.createAlert(alertAddDTO, interestedUsers);
+    }
 }
