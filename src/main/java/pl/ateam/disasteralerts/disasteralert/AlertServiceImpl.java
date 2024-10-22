ef241360dee1ce367236de22c4ebbc214741a4c9
jkuznik <janusz.kuznik89@gmail.com>
2024-10-22 19:52:38 +0200

    Add UserFacadeWydmuszka to achieve needed data, add missing logic to achieve send notification

--- src/main/java/pl/ateam/disasteralerts/disasteralert/AlertServiceImpl.java
+++ src/main/java/pl/ateam/disasteralerts/disasteralert/AlertServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pl.ateam.disasteralerts.disasteralert.dto.AlertAddDTO;
 import pl.ateam.disasteralerts.disasteralert.dto.AlertDTO;
+import pl.ateam.disasteralerts.user.UserFacadeWydmuszka;
 import pl.ateam.disasteralerts.user.dto.UserDTO;
 
 import java.util.Set;
@@ -16,6 +17,7 @@ class AlertServiceImpl implements AlertService {
     private final AlertRepository repository;
     private final AlertManager alertManager;
     private final NotificationService notificationService;
+    private final UserFacadeWydmuszka userFacadeWydmuszka;
     private final AlertMapper mapper;
 
     @Transactional
