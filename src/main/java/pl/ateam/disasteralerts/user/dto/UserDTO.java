eaffa2e484b9ca947b683cb033e67e422e3cecd8
YamiHeike <koscielniak.z@gmail.com>
2024-10-22 22:17:58 +0200

    feat: added password validation

--- src/main/java/pl/ateam/disasteralerts/user/dto/UserDTO.java
+++ src/main/java/pl/ateam/disasteralerts/user/dto/UserDTO.java
@@ -1,9 +1,6 @@
 package pl.ateam.disasteralerts.user.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import pl.ateam.disasteralerts.user.Role;
 
@@ -15,6 +12,10 @@ public record UserDTO(
                         @Max(255)
                         String email,
                         @NotBlank @Size(min = 6, max = 255)
+                        @Pattern(
+                                regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?!.* ).{6,25}$",
+                                message = "Hasło musi zawierać co najmniej jedną cyfrę, małą literę, wielką literę, znak specjalny oraz mieścić się w przedziale 6-25 znaków"
+                        )
                         String password,
                         @Size(min=9,max=9)
                         String phoneNumber,
--- src/main/java/pl/ateam/disasteralerts/user/dto/UserLoginDTO.java
+++ src/main/java/pl/ateam/disasteralerts/user/dto/UserLoginDTO.java
@@ -1,15 +1,13 @@
 package pl.ateam.disasteralerts.user.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record UserLoginDTO(
         @Email @NotBlank
         @Max(255)
         String email,
         @NotBlank
-        @Size(min=6,max=255)
+        @Size(min=6,max=25)
+        @Pattern(regexp = "Password must contain at least one digit, one lowercase letter, one uppercase letter, one special character, and be between 8 and 16 characters long")
         String password) {
 }
