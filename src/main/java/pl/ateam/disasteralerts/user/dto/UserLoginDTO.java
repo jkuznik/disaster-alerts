f7fe871289533c2ac2f24291d43af492769ead6a
YamiHeike <koscielniak.z@gmail.com>
2024-10-23 18:15:57 +0200

    fixed: lack of regex check in UserLoginDTO validation

--- src/main/java/pl/ateam/disasteralerts/user/dto/UserLoginDTO.java
+++ src/main/java/pl/ateam/disasteralerts/user/dto/UserLoginDTO.java
@@ -9,6 +9,8 @@ public record UserLoginDTO(
         String location,
         @NotBlank
         @Size(min=6,max=25)
-        @Pattern(regexp = "",  message = "Password must contain at least one digit, one lowercase letter, one uppercase letter, one special character, and be between 8 and 16 characters long")
+        @Pattern(
+                regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?!.* ).{6,25}$",
+                message = "Password must contain at least one digit, one lowercase letter, one uppercase letter, one special character, and be between 8 and 16 characters long")
         String password) {
 }
