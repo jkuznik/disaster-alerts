ebca35f0aafd439af24c9304d42a1816cdc42f28
Zuzanna <122471621+YamiHeike@users.noreply.github.com>
2024-10-23 18:12:46 +0200

    Update src/main/java/pl/ateam/disasteralerts/user/dto/UserLoginDTO.java
    Co-authored-by: Slawek84PL <93817686+Slawek84PL@users.noreply.github.com>

--- src/main/java/pl/ateam/disasteralerts/user/dto/UserLoginDTO.java
+++ src/main/java/pl/ateam/disasteralerts/user/dto/UserLoginDTO.java
@@ -9,6 +9,6 @@ public record UserLoginDTO(
         String location,
         @NotBlank
         @Size(min=6,max=25)
-        @Pattern(regexp = "Password must contain at least one digit, one lowercase letter, one uppercase letter, one special character, and be between 8 and 16 characters long")
+        @Pattern(regexp = "",  message = "Password must contain at least one digit, one lowercase letter, one uppercase letter, one special character, and be between 8 and 16 characters long")
         String password) {
 }
