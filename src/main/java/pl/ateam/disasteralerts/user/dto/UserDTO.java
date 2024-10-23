6a68ece8538ba38d5027aea257977c53169f566d
YamiHeike <koscielniak.z@gmail.com>
2024-10-23 18:22:47 +0200

    added regex check in phone number validation

--- src/main/java/pl/ateam/disasteralerts/user/dto/UserDTO.java
+++ src/main/java/pl/ateam/disasteralerts/user/dto/UserDTO.java
@@ -17,7 +17,10 @@ public record UserDTO(
                                 message = "Hasło musi zawierać co najmniej jedną cyfrę, małą literę, wielką literę, znak specjalny oraz mieścić się w przedziale 6-25 znaków"
                         )
                         String password,
-                        @Size(min=9,max=9)
+                        @Pattern(
+                                regexp = "^\\\\d{9}$\"",
+                                message = "Nieprawidłowy format numeru telefonu"
+                        )
                         String phoneNumber,
                         @Max(255)
                         @NotBlank
