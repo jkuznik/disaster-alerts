8964d36d185f854783fc012546b1d55ecaf2bf74
RobertoJavaDev <robertojavadev@gmail.com>
2024-10-25 09:13:08 +0200

    feat: Added another enum type for disaster type

--- src/main/java/pl/ateam/disasteralerts/disasteralert/DisasterType.java
+++ src/main/java/pl/ateam/disasteralerts/disasteralert/DisasterType.java
@@ -1,5 +1,5 @@
 package pl.ateam.disasteralerts.disasteralert;
 
 public enum DisasterType {
-    STORM, FIRE, FLOOD, HURRICANE, BLIZZARD, DROUGHT
+    STORM, FIRE, FLOOD, HURRICANE, BLIZZARD, DROUGHT, HEAT
 }
