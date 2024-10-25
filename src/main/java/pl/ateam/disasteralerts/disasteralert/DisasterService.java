61eb2b69821380eff2854ca30e1a071c5d4d7e62
RobertoJavaDev <robertojavadev@gmail.com>
2024-10-25 09:15:50 +0200

    feat: Added application config

--- src/main/java/pl/ateam/disasteralerts/disasteralert/DisasterService.java
+++ src/main/java/pl/ateam/disasteralerts/disasteralert/DisasterService.java
@@ -6,9 +6,11 @@ import org.springframework.validation.annotation.Validated;
 import pl.ateam.disasteralerts.disasteralert.dto.DisasterAddDTO;
 import pl.ateam.disasteralerts.disasteralert.dto.DisasterDTO;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Validated
 public interface DisasterService {
     DisasterDTO addDisaster(@NotNull @Valid DisasterAddDTO disasterAddDTO);
+    Optional<DisasterDTO> getActiveDisasterForTypeAndLocation(@NotNull @Valid DisasterType type, String location);
 }
