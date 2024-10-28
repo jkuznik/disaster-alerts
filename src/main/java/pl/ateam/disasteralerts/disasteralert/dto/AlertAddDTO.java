179ab66113ec2d3bfb68c57c11c5a961e603235e
jkuznik <janusz.kuznik89@gmail.com>
2024-10-28 18:57:00 +0100

    Update Alert entity

--- src/main/java/pl/ateam/disasteralerts/disasteralert/dto/AlertAddDTO.java
+++ src/main/java/pl/ateam/disasteralerts/disasteralert/dto/AlertAddDTO.java
@@ -8,7 +8,6 @@ import java.util.UUID;
 public record AlertAddDTO(@NotNull UUID id,
                           @NotNull UUID disasterId,
                           @NotNull String description,
-                          @NotNull String location,
-                          @NotNull LocalDateTime creationDate) {
+                          @NotNull String location) {
 }
 
