09ada14927bb14c77649d3366ed6111a9581fc29
RobertoJavaDev <robertojavadev@gmail.com>
2024-10-30 10:47:53 +0100

    feat: Added Open AI Client

--- src/main/java/pl/ateam/disasteralerts/airiskassessment/OpenAIClient.java
+++ src/main/java/pl/ateam/disasteralerts/airiskassessment/OpenAIClient.java
@@ -1,4 +1,19 @@
 package pl.ateam.disasteralerts.airiskassessment;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@RequiredArgsConstructor
 class OpenAIClient {
+
+    private final RestTemplate restTemplate;
+
+    double getRiskScore(String prompt) {
+        JsonNode response = restTemplate.getForObject(null, JsonNode.class);
+
+        return response.path("choices").get(0).path("score").asDouble();
+    }
 }
