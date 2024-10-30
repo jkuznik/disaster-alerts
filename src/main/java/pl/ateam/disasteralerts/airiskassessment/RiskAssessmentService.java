e5a6568d87b2e6b91e6677bdb5c417f18c25968c
RobertoJavaDev <robertojavadev@gmail.com>
2024-10-30 10:46:56 +0100

    feat: Implemented risk assessment service

--- src/main/java/pl/ateam/disasteralerts/airiskassessment/RiskAssessmentService.java
+++ src/main/java/pl/ateam/disasteralerts/airiskassessment/RiskAssessmentService.java
@@ -1,4 +1,30 @@
 package pl.ateam.disasteralerts.airiskassessment;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.ateam.disasteralerts.disasteralert.dto.DisasterAddDTO;
+
+@Service
+@RequiredArgsConstructor
 class RiskAssessmentService {
+
+    private final OpenAIClient openAIClient;
+
+    DisasterAddDTO assessRisk(DisasterAddDTO disasterAddDTO) {
+        String riskEvaluationPrompt = String.format(
+                "Evaluate the risk for the following disaster data:\n"
+                        + "Location: %s\nType: %s\nDescription: %s\nReported By: %s\n",
+                disasterAddDTO.location(),
+                disasterAddDTO.description(),
+                disasterAddDTO.type()
+        );
+
+        double riskScore = openAIClient.getRiskScore(riskEvaluationPrompt);
+
+        if (riskScore > 0.7) {
+
+        }
+
+        return disasterAddDTO;
+    }
 }
