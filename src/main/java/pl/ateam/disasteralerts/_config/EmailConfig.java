c3f0c02354bb3ab5cb984c1b5b9e293f8779d7ba
jkuznik <janusz.kuznik89@gmail.com>
2024-10-31 08:14:46 +0100

    Add EmailCongig class

---
+++ src/main/java/pl/ateam/disasteralerts/_config/EmailConfig.java
@@ -0,0 +1,41 @@
+package pl.ateam.disasteralerts._config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.Properties;
+
+@EnableAsync
+@EnableRetry
+@Configuration
+public class EmailConfig {
+
+    @Value("${spring.mail.username}")
+    String username;
+
+    @Value("${spring.mail.password}")
+    String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+    }
+}
