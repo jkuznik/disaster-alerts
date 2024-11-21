5b28eb7c599b787dbfca55e75299bfce6fe5715b
Slawek84PL <slapyt84@gmail.com>
2024-11-21 21:05:15 +0100

    Added disaster list view in controller

--- src/main/java/pl/ateam/disasteralerts/web/DisasterViewController.java
+++ src/main/java/pl/ateam/disasteralerts/web/DisasterViewController.java
@@ -10,13 +10,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.ateam.disasteralerts.disasteralert.DisasterAlertFacade;
 import pl.ateam.disasteralerts.disasteralert.DisasterStatus;
 import pl.ateam.disasteralerts.disasteralert.DisasterType;
 import pl.ateam.disasteralerts.disasteralert.dto.DisasterAddDTO;
+import pl.ateam.disasteralerts.disasteralert.dto.DisasterDTO;
 import pl.ateam.disasteralerts.security.AppUser;
 import pl.ateam.disasteralerts.util.CitiesInPoland;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
@@ -54,6 +60,44 @@ public class DisasterViewController {
         disasterAlertFacade.createDisaster(disasterAddDTO, USER_AS_DISASTER_SOURCE);
         redirectAttributes.addFlashAttribute("message", "Dodano zdarzenie");
         return "redirect:/disasters/add";
+    }
+
+    @GetMapping("list")
+    public String showDisasterList(Model model, @AuthenticationPrincipal AppUser userDetails) {
+
+        baseModel(model, userDetails);
+
+        DisasterDTO disasterDTO1 = new DisasterDTO(UUID.randomUUID(),
+                DisasterType.FIRE,
+                "Opis katastrofy ogniowej",
+                "user",
+                "Bielsko-Biała",
+                LocalDateTime.now().minusDays(2),
+                LocalDateTime.now().plusDays(3),
+                DisasterStatus.ACTIVE,
+                userDetails.getUserDTO().id());
+        DisasterDTO disasterDTO2 = new DisasterDTO(UUID.randomUUID(),
+                DisasterType.HURRICANE,
+                "Opis katastrofy wiatrowej",
+                "user",
+                "Bielsko-Biała",
+                LocalDateTime.now().minusDays(1),
+                LocalDateTime.now().plusDays(1),
+                DisasterStatus.ACTIVE,
+                userDetails.getUserDTO().id());
+
+        if (!model.containsAttribute("list")) {
+            model.addAttribute("list", List.of(disasterDTO1, disasterDTO2));
+        }
+        return "listDisasters";
+    }
+
+    @PostMapping("list")
+    public String filterList(@RequestParam(name = "disasterType", value = "") String disasterType,
+                             @RequestParam(name = "city", value = "") String city,
+                             RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("list", null);
+        return "redirect:/disasters/list";
     }
 
     private void baseModel(Model model, AppUser userDetails) {
