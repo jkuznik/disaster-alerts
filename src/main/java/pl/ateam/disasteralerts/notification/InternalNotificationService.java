96124d1426fc74ed4a8c7fff080ea7919845eb04
jkuznik <janusz.kuznik89@gmail.com>
2024-12-12 20:45:24 +0100

    Logic to insert IN into db and get all IN by userId

--- src/main/java/pl/ateam/disasteralerts/notification/InternalNotificationService.java
+++ src/main/java/pl/ateam/disasteralerts/notification/InternalNotificationService.java
@@ -6,21 +6,25 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.mapstruct.Mapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import pl.ateam.disasteralerts.alert.dto.AlertAddDTO;
+import pl.ateam.disasteralerts.notification.dto.InternalNotificationDTO;
 import pl.ateam.disasteralerts.user.dto.UserDTO;
 import pl.ateam.disasteralerts.util.EntityAudit;
 
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 class InternalNotificationService implements NotificationListener{
 
     private final InternalNotificationRepository internalNotificationRepository;
+    private final InternalNotificationMapper internalNotificationMapper;
 
     @Override
     public void addedAlert(AlertAddDTO alertAddDTO, UserDTO interestedUser) {
@@ -30,6 +34,12 @@ class InternalNotificationService implements NotificationListener{
                         .alertDescription(alertAddDTO.description())
                         .alreadyRead(false)
                 .build());
+    }
+
+    public Set<InternalNotificationDTO> getAllIN(UUID userId) {
+        return internalNotificationRepository.findAllByUserId(userId).stream()
+                .map(internalNotificationMapper::toDto)
+                .collect(Collectors.toSet());
     }
 }
 
@@ -53,6 +63,11 @@ class InternalNotification extends EntityAudit {
 @Repository
 interface InternalNotificationRepository extends JpaRepository<InternalNotification, UUID> {
     Set<InternalNotification> findAllByUserId(UUID userId);
+}
+
+@Mapper(componentModel = "spring")
+interface InternalNotificationMapper {
+    InternalNotificationDTO toDto(InternalNotification internalNotification);
 }
 
 
--- src/main/java/pl/ateam/disasteralerts/notification/NotificationFacade.java
+++ src/main/java/pl/ateam/disasteralerts/notification/NotificationFacade.java
@@ -3,16 +3,25 @@ package pl.ateam.disasteralerts.notification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import pl.ateam.disasteralerts.alert.dto.AlertAddDTO;
+import pl.ateam.disasteralerts.notification.dto.InternalNotificationDTO;
 import pl.ateam.disasteralerts.user.dto.UserDTO;
+
+import java.util.Set;
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
 public class NotificationFacade {
 
     private final NotificationManager notificationManager;
+    private final InternalNotificationService internalNotificationService;
 
     public void sendNotifications(AlertAddDTO alertAddDTO, UserDTO interestedUser) {
         notificationManager.sendNotifications(alertAddDTO, interestedUser);
+    }
+
+    public Set<InternalNotificationDTO> getAllInternalNotifications (UUID userId){
+        return internalNotificationService.getAllIN(userId);
     }
 
     public void addEmailService() {
